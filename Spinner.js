5ceaef84843066580a550fd19c495141a9b2a89f
zm00622 <56224858+zm00622@users.noreply.github.com>
2019-12-21 16:07:12 -0500

    Create Spinner.js
    This code was created while following Section 6 of the Modern React with Redux Course by Stephen Grider. 
    
    In this part of the course, I learned the reasons for why one might use class components instead of functional components.

---
+++ Spinner.js
@@ -0,0 +1,17 @@
+// This file creates a load screen.
+
+import React from 'react';
+
+const Spinner = (props) => {
+  return (
+    <div className="ui active dimmer">
+      <div className="ui big text loader">{props.message}</div>
+    </div>
+  );
+};
+
+Spinner.defaultProps = {
+  message: 'Loading;'
+}
+
+export default Spinner;
